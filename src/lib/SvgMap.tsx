66366745986b94d3303f13d6b0152e078c20e300
S. Suzuki <terrierscript@gmail.com>
2022-06-23 18:44:30 +0900

    add colors

--- src/lib/SvgMap.tsx
+++ src/lib/SvgMap.tsx
@@ -1,29 +1,44 @@
-import { FC } from "react"
-import { WeekResult } from "./github"
+import { FC, useMemo } from "react"
+import { ContributionLevel, WeekResult } from "./github"
 
 type RectProps = {
   x: number
   y: number
   size: number
-  level: number
+  level: ContributionLevel
 }
 const RectCell: FC<RectProps> = ({ x, y, size, level }) => {
-  const levelToColor = level > 0 ? "#ffffff" : "#000000"
+  const levelToColor = useMemo(() => {
+    switch (level) {
+      case "NONE":
+        return "#fff3"
+      case "FIRST_QUARTILE":
+        return "#fff6"
+      case "SECOND_QUARTILE":
+        return "#fff9"
+      case "THIRD_QUARTILE":
+        return "#fffd"
+      case "FOURTH_QUARTILE":
+        return "#fff"
+    }
+  }, [level])
   return <rect {...{ x, y }}
     height={size}
     width={size}
     fill={levelToColor}
     rx={3} ry={3} />
 }
-const RectCellMap: FC<{ week: WeekResult }> = ({ week }) => {
-  const size = 10
-  const gap = 2
-  const padding = 2
+const RectCellMap: FC<{
+  week: WeekResult,
+  size: number,
+  gap: number,
+  padding: number,
+}> = ({ week, size, gap, padding }) => {
   const cells = week.map((w, weekIdx) => {
     return w.contributionDays.map((day, dayIdx) => {
       const wd = dayIdx
       const w = weekIdx
-      const level = day.contributionCount > 0 ? 1 : 0
+      const level = day.contributionLevel
       return { w, wd, level }
     })
   }).flat(1)
@@ -47,26 +62,34 @@ export const SvgMap: FC<{
   imageSrc: string,
   week: WeekResult
 }> = ({ imageSrc, week }) => {
-
+  const size = 10
+  const gap = 1
+  const padding = size * 2
+  const { width, height } = {
+    width: (week.length + 1) * (size + gap) + padding * 2,
+    height: 7 * (size + gap) + padding * 2,
+  }
   return <svg
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     // xmlns:xlink="http://www.w3.org/1999/xlink"
-    width={600} height={300}>
+    {...{ width, height }}
+  >
     <defs>
       <mask id="mask">
-        <RectCellMap week={week} />
+        <rect x="0" y="0" width={width} height={height} fill="#ffffff22" />
+        <RectCellMap week={week} size={size} gap={gap} padding={padding} />
       </mask>
     </defs>
     <image
       mask={"url(#mask)"}
-      width={600} height={300}
+      {...{ width, height }}
       href={imageSrc}
-      preserveAspectRatio="xMinYMin meet" />
+      preserveAspectRatio="xMidYMid slice" />
   </svg>
 }
 
-export const DemoSvgMap = () => {
-  const imageSrc = "/demo.jpg"
-  return <SvgMap imageSrc={imageSrc} />
-}
+// export const DemoSvgMap = () => {
+//   const imageSrc = "/demo.jpg"
+//   return <SvgMap imageSrc={imageSrc} />
+// }
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,8 +1,11 @@
 import { graphql } from "@octokit/graphql"
 
+export type ContributionLevel = "NONE" | "FIRST_QUARTILE" | "SECOND_QUARTILE" | "THIRD_QUARTILE" | "FOURTH_QUARTILE"
+
 export type WeekResult = {
   contributionDays: {
     contributionCount: number,
+    contributionLevel: ContributionLevel
     date: string
   }[]
 }[]
@@ -17,6 +20,7 @@ export type GraphResult = {
     }
   }
 }
+
 export const userGraph = async (username: string): Promise<WeekResult> => {
   const result = await graphql<GraphResult>(`
     query($userName:String!) { 
@@ -27,6 +31,7 @@ export const userGraph = async (username: string): Promise<WeekResult> => {
             weeks {
               contributionDays {
                 contributionCount
+                contributionLevel
                 date
               }
             }
