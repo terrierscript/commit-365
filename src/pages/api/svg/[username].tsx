0c05baaed57e6d11400bd549ce30639c4bdb0d4f
S. Suzuki <terrierscript@gmail.com>
2022-06-24 00:45:49 +0900

    inject base64 img

--- src/pages/api/svg/[username].tsx
+++ src/pages/api/svg/[username].tsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { fileTypeFromBuffer } from "file-type"
 import { NextApiHandler } from "next"
 
 import * as ReactDOMServer from 'react-dom/server'
@@ -10,8 +12,17 @@ const handler: NextApiHandler = async (req, res) => {
 
   const week = await getUserContributionWeekGraph(username, Number(day) || 365)
 
-  const proxyUrl = `/api/img?url=${url}`
-  const domSvg = ReactDOMServer.renderToString(<SvgMap imageSrc={proxyUrl} week={week} />)
+  const data = await axios.get(url, {
+    responseType: "arraybuffer"
+  }).then(res => res.data)
+  const fileType = await fileTypeFromBuffer(data)
+
+  // const proxyUrl = `/api/img?url=${url}`
+  const img = `data:${fileType?.mime};base64,${data.toString("base64")}`
+
+  const domSvg = ReactDOMServer.renderToString(<SvgMap
+    imageSrc={img}
+    week={week} />)
   res
     .status(200)
     .setHeader("Content-Type", "image/svg+xml")
